20e03e5bcb8afb9476948775bfcbb9c4604b43ae
ethicnology <ethicnology@pm.me>
2020-09-25 15:05:52 +0200

    Firebase auth

--- app/src/main/java/com/ethicnology/symbionte/MainActivity.kt
+++ app/src/main/java/com/ethicnology/symbionte/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ethicnology.symbionte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
 }
