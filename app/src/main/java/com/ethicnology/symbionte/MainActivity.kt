bf69f74fad3bced187d245a850381dd0dd269e28
ethicnology <ethicnology@pm.me>
2020-09-25 15:58:29 +0200

    sign up & log in

--- app/src/main/java/com/ethicnology/symbionte/MainActivity.kt
+++ app/src/main/java/com/ethicnology/symbionte/MainActivity.kt
@@ -2,7 +2,12 @@ package com.ethicnology.symbionte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -15,5 +20,64 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // Initialize Firebase Auth
         auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?){
+        if(user != null){
+            findViewById<EditText>(R.id.editTextEmailAddress).visibility = View.VISIBLE
+            findViewById<EditText>(R.id.editTextPassword).visibility = View.VISIBLE
+        } else {
+            findViewById<EditText>(R.id.editTextEmailAddress).visibility = View.VISIBLE
+            findViewById<EditText>(R.id.editTextPassword).visibility = View.VISIBLE
+        }
+    }
+
+    public fun buttonSignUp(view: View){
+        val email = findViewById<EditText>(R.id.editTextEmailAddress).toString()
+        val password = findViewById<EditText>(R.id.editTextPassword).toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Auth", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Auth", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    public fun buttonLogIn(view: View){
+        val email = findViewById<EditText>(R.id.editTextEmailAddress).text.toString()
+        val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
+
+        Toast.makeText(this, email,Toast.LENGTH_SHORT).show()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Auth", "logInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Auth", "logInWithEmail:failure", task.exception)
+                    updateUI(null)
+                }
+            }
     }
 }
