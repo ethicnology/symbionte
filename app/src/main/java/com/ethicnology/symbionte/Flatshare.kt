1b3e8ebe4496ebeb0c43ad0f781ed383466e1f0f
ethicnology <ethicnology@pm.me>
2020-11-06 14:35:00 +0100

    Add Flatshare Activity Create new activity Add Parent activity to manifest Add this activity to Incipit

---
+++ app/src/main/java/com/ethicnology/symbionte/Flatshare.kt
@@ -0,0 +1,11 @@
+package com.ethicnology.symbionte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Flatshare : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.flatshare)
+    }
+}
