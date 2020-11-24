83fa0f1eea4c243e408bd2da6c33da8fb61a45fa
ethicnology <ethicnology@pm.me>
2020-11-24 18:47:12 +0100

    Add Location Activity reference location in Incipit recycler view update gradle

---
+++ app/src/main/java/com/ethicnology/symbionte/Location.kt
@@ -0,0 +1,11 @@
+package com.ethicnology.symbionte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Location : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.location)
+    }
+}
